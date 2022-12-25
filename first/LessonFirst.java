1979bc04422a9821bbf865ba2ba7e53e9cb8e39b
azolanda <uboikov@yandex.ru>
2022-12-26 01:18:28 +0300

    second task added

--- first/LessonFirst.java
+++ first/LessonFirst.java
@@ -4,20 +4,35 @@ import java.util.Scanner;
 
 /**
  * 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n)
+ * 2. Вычислить n! (произведение чисел от 1 до n)
  */
 
 public class LessonFirst {
 
     public static void main(String[] args) {
-        System.out.printf("n-ое треугольное число равно %d \n", task_1());
+        // System.out.printf("n-ое треугольное число равно %d \n",
+        // taskFirst(readNum()));
+        System.out.printf("Факториал числа n равен %d \n",
+                taskSecondFact(readNum()));
     }
 
-    public static int task_1() {
+    public static int readNum() {
         System.out.println("Введите натуральное число n: -->");
         Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
-        double result = 0.5 * num * (num + 1);
         scan.close();
+        return num;
+    }
+
+    public static int taskFirst(int n) {
+        double result = 0.5 * n * (n + 1);
         return (int) result;
+    }
+
+    public static int taskSecondFact(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        int fact = n * taskSecondFact(n - 1);
+        return fact;
     }
 }
